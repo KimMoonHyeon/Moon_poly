10387b0c4b78f94087f57a059e41887966746da3
KimMoonHyeon <93696279+KimMoonHyeon@users.noreply.github.com>
2021-11-25 00:14:02 +0900

    GO

---
+++ Assets/Moon_Script/Season_ob_S.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Season_ob_S : MonoBehaviour {
+
+	public GameObject season_ob;
+	private float ob_up_time;
+	Vector3 season_ob_pos;
+	void Start () {
+		ob_up_time = 0f;
+	}
+	
+	
+	void Update () {
+
+		if (ob_up_time < 1.5)
+		{
+			season_ob.transform.position = new Vector3(season_ob.transform.position.x, season_ob.transform.position.y + ob_up_time* Time.deltaTime, season_ob.transform.position.z);
+			ob_up_time += Time.deltaTime;
+			Debug.Log(ob_up_time);
+		}
+	}
+}
